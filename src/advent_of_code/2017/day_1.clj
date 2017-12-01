e653c7b28bae278318c5102f2a069f49d03113ae
Nikos Fertakis <nikos@skroutz.gr>
2017-12-01 15:52:30 +0200

    Add day1 for advent 2017

---
+++ src/advent_of_code/2017/day_1.clj
@@ -0,0 +1,29 @@
+(ns advent-of-code.2017.day-1)
+
+(def inpt
+  (let [data (slurp "resources/2017/day_1_input.txt")]
+    (take (dec (count data)) data)))
+
+(defn cycle-plus-1 [a]
+  (take (inc (count a)) (cycle a)))
+
+(defn conseq-pairs [a]
+  (partition 2 1 (cycle-plus-1 a)))
+
+(defn add-if-conseq [a]
+  (reduce
+   (fn [n [a b]] (+ n (if (= a b) (Character/getNumericValue a) 0)))
+   0
+   a))
+
+(defn first-star []
+  (add-if-conseq (conseq-pairs inpt)))
+
+(defn pair-nth [a]
+  (map
+   (fn [a b] [a b])
+   a
+   (take (count a) (drop (/ (count a) 2) (cycle a)))))
+
+(defn second-star []
+  (add-if-conseq (pair-nth inpt)))
