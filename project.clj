0d33b2f80fd50ddc371e16da2e176612b3a6dc84
Nikos Fertakis <nikos@skroutz.gr>
2017-12-01 15:51:56 +0200

    Update to clojure 1.9

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Advent of code solutions in Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
                  [pandect "0.5.4"]
                  [net.mikera/core.matrix "0.47.0"]]
   :target-path "target/%s"
